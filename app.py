c8172ec5790c4e505069f2809470cefa24949875
kazuyuk <37919061+kazuyuk@users.noreply.github.com>
2024-07-24 14:42:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import time
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World!'
+
+@app.route('/sleep')
+def sleep():
+    time.sleep(5)  # 5秒間待機
+    return 'Slept for 5 seconds!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
